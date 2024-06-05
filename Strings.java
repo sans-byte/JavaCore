2034e3b3e925c3ee6f9bde46bddfc137a296a3ac
sans-byte <sanskarj15@gmail.com>
2024-06-05 11:37:29 +0530

    Mutable  strings

--- Strings.java
+++ Strings.java
@@ -15,6 +15,7 @@ public class Strings {
         // Mutable strings and Immuatable strings
 
         // Strings are Immutable by default
+
         // And There cannot  be two string of the same name in string pool which is in heap memory
         // ex String name  = "Sanskar"
         //    String newName = "Sanskar"
@@ -34,6 +35,32 @@ public class Strings {
         s1 = s1 + "Jain";
         System.out.println(s1);
         System.out.println(s1.hashCode()); // prints address
+
+
+        // TO use mutable strings we use something known as 
+        // String buffer or String Builder
+
+        //String Buffer
+
+        StringBuffer s = new StringBuffer("Sanskar");
+        System.out.println(s.length()); //7
+        System.out.println(s.capacity()); //23
+
+        s.append(" Jain");
+        System.out.println(s);
+        // String str = s.toString(); //convert stringbuffer to string
+        // s.delete(2, 4);
+        // System.out.println(s); //Sakar Jain
+
+        // s.deleteCharAt(3);
+        // System.out.println(s); // Sankar Jain
+
+        //s.insert(8, "Hello world ");
+
+        //System.out.println(s); // Sanskar Hello world Jain
+
+        //String buffer and String builder has  only one difference that
+        // String buffer is threas safe and string builder is not
     }
 
 }
