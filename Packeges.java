113f2586db3b56e8cf068b18889b39fd22610cc7
sans-byte <sanskarj15@gmail.com>
2024-06-07 11:49:54 +0530

    packages in java

---
+++ Packeges.java
@@ -0,0 +1,13 @@
+// Packages in java are used to import the extra functionality
+// or the code which is pre written
+// The package name should be unique so the best way is
+// reverse your company's domain name and use it
+// com.google.something -> this will be unique
+
+// tools.*                  //these both are not same
+// tools.sometool.*         // as * only import all files in current folder not all folders
+
+
+public class Packeges {
+    
+}
