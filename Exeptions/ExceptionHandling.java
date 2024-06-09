8cadbd0bcbd1ddcb4d7bff0d719e9c26f0667b0b
sans-byte <sanskarj15@gmail.com>
2024-06-09 10:35:41 +0530

    exception handling basics

---
+++ Exeptions/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package Exeptions;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int i = 0;
+        try {
+            int j = 18 / i; // Now here we can get runtime error
+            System.out.println(j);
+        } catch (Exception e) {
+            System.out.println("Something went wrong...");
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ Exeptions/WhatAreExceptions.java
@@ -0,0 +1,13 @@
+
+
+package Exeptions;
+
+// Types of errors -> Compile Time, Runtime, Logical
+// Compile time -> Compiler will help you with those while compiling the code
+// Logical -> While runnning code we can literally see the output and fix logical errors
+// Runtime -> They occur on run time and stop the execution of the code so they must be 
+// handled so here we use Exception handling.
+
+public class WhatAreExceptions {
+    
+}
