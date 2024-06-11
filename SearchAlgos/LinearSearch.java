ff7a5f2de27fc2e41d375261cbb9aa874c67f6d6
sans-byte <sanskarj15@gmail.com>
2024-06-11 19:07:50 +0530

    linear search algo

---
+++ SearchAlgos/LinearSearch.java
@@ -0,0 +1,30 @@
+package SearchAlgos;
+
+import java.util.ArrayList;
+
+public class LinearSearch {
+    public static void main(String[] args){
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(2);
+        arr.add(4);
+        arr.add(26);
+        arr.add(9);
+        arr.add(3);
+        System.out.println(linearSearch(arr, 9));
+    }
+    //Time complexity worst - O(n)
+    
+    static int linearSearch(ArrayList<Integer> arr, int target){
+        // Edge case
+        if(arr.size() <= 0){
+            return -1;
+        }
+
+        for(int i=0;i< arr.size();i++){
+            if(arr.get(i) == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
