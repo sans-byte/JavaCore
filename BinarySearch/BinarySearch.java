16d0694fd1a644f3eb6fc32d5ca09f53b455726f
sans-byte <sanskarj15@gmail.com>
2024-06-13 21:32:00 +0530

    search in rotated array

--- BinarySearch/BinarySearch.java
+++ BinarySearch/BinarySearch.java
@@ -3,7 +3,7 @@ package BinarySearch;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50,60,70,80,90};
-        int target = 1000;
+        int target = 70;
         System.out.println(binarySearch(arr,0,8,target));
     }
 
@@ -12,7 +12,7 @@ public class BinarySearch {
         if(arr == null || arr.length <=0 || j < i) return -1; 
         int mid = i + (j - i)/2; //this won't exceed the limit
         if(arr[mid] == target) return mid;
-        if(arr[mid] > target) return binarySearch(arr,i,mid ,target);
+        if(arr[mid] > target) return binarySearch(arr,i,mid-1 ,target);
         if(arr[mid] < target) return binarySearch(arr, mid+1, j, target);
         return -1;
     }
---
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,60 @@
+package BinarySearch;
+
+// Given a rotated sorted array ex : {4,5,6,7,0,1,2} and a target find target in the array
+
+// Approach 1;
+// find the pivot( basically the largest value in the array ) and apply binary search in left and 
+// righ array start to pivot and pivot to end
+// ** How to find the pivot element **
+//
+//
+// if(arr[start] <= arr[mid]) then all the elements after mid are smaller than start to discard 
+// those start = mid
+// else if(arr[start] > arr[mid]) then all the elements before mid are smaller than it so 
+// discard those end = mid-1
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {2,5,6,0,0,1,2};
+        int target = 0;
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+        if (pivot == -1)
+            System.out.println(binarySearch(arr, 0, arr.length - 1, target));
+        else {
+            int value1 = binarySearch(arr, 0, pivot, target);
+            if (value1 != -1)
+                System.out.println(value1);
+            int value2 = binarySearch(arr, pivot+1, arr.length - 1, target);
+            System.out.println(value2);
+        }
+    }
+
+    static int findPivot(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            if (m > s && arr[m] < arr[m - 1]) {
+                return m - 1;
+            }
+            if (m < e && arr[m] > arr[m + 1]) {
+                return m;
+            }
+            if (arr[s] >= arr[m]) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] arr,int i,int j,int target){
+        if(arr == null || arr.length <=0 || j < i) return -1; 
+        int mid = i + (j - i)/2; //this won't exceed the limit
+        if(arr[mid] == target) return mid;
+        if(arr[mid] > target) return binarySearch(arr,i,mid-1 ,target);
+        if(arr[mid] < target) return binarySearch(arr, mid+1, j, target);
+        return -1;
+    }
+}
