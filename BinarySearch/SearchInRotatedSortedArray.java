986c7220811bffce9f61193762f636efab8ea43a
sans-byte <sanskarj15@gmail.com>
2024-06-13 22:46:59 +0530

    search in rotated array

--- BinarySearch/SearchInRotatedSortedArray.java
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -14,7 +14,7 @@ package BinarySearch;
 // discard those end = mid-1
 public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
-        int[] arr = {2,5,6,0,0,1,2};
+        int[] arr = {4,5,6,7,0,1,2};
         int target = 0;
         int pivot = findPivot(arr);
         System.out.println(pivot);
