10cbdb25fb647868531d933880435f7909aee475
sans-byte <sanskarj15@gmail.com>
2024-06-15 21:26:20 +0530

    Search in 2d sorted array

--- BinarySearch/SearchIn2DMatrix.java
+++ BinarySearch/SearchIn2DMatrix.java
@@ -1,5 +1,6 @@
 package BinarySearch;
 
+//Given the condition that the array is sorted
 public class SearchIn2DMatrix {
     public static void main(String[] args) {
         int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
