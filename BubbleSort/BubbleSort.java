f35d74ba25476b85e4af17b59b9ff216dd96f0e0
sans-byte <sanskarj15@gmail.com>
2024-06-16 23:01:05 +0530

    BubbleSort algo structure

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,15 @@
+package BubbleSort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 6, 1, 9, 7, 12, 14, 15, 11, 25, 22, 24 };
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+
+    static int[] bubbleSort(int[] arr) {
+
+        return arr;
+    }
+}
