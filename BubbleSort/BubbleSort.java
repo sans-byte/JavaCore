2bca2e092d8041e62db72fe420ef15e4476beaf5
sans-byte <sanskarj15@gmail.com>
2024-06-16 23:13:05 +0530

    Added some more comments and facts about the algo

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 // Time Best Case = O(N) Worst Case = O(N2)
 //AKA sinking sort and exchange sort
+// It is a stable sorting algorithm which means the order after sorting is maintained
+// EX - R-red, B-black {10(B),20(B),30(B),10(R),30(R),20(R)} after sorting = {10(B),10(R),20(B),20(R),30(B),30(R)}
 
 public class BubbleSort {
     public static void main(String[] args) {
