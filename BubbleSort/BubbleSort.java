1e621241b01bdb5b3e051aeb65032bc8ee1a99aa
sans-byte <sanskarj15@gmail.com>
2024-06-16 23:17:49 +0530

    Bubble sort algo

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -10,11 +10,20 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = { 3, 5, 6, 1, 9, 7, 12, 14, 15, 11, 25, 22, 24 };
-        System.out.println(Arrays.toString(bubbleSort(arr)));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static int[] bubbleSort(int[] arr) {
-
-        return arr;
+    static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                // We do check and swapping here
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                }
+            }
+        }
     }
 }
