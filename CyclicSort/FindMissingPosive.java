c46c7f92562762a26726b1214b5b163973b39577
sans-byte <sanskarj15@gmail.com>
2024-06-18 23:05:30 +0530

    find missing positive

--- CyclicSort/FindMissingPosive.java
+++ CyclicSort/FindMissingPosive.java
@@ -1,5 +1,7 @@
 package CyclicSort;
 
+import java.util.Arrays;
+
 //https://leetcode.com/problems/first-missing-positive/
 // Leetcode hard question
 // In this question it's not given range (1,n) but after seeing the question we can 
@@ -7,6 +9,35 @@ package CyclicSort;
 
 public class FindMissingPosive {
     public static void main(String[] args) {
-        
+        int[] nums = { 3, 4, -1, 1 };
+        findCorrectIndex(nums);
+        System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                System.out.println(i+1);
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    static void findCorrectIndex(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] <= 0 || arr[i] > arr.length)
+                i++;
+            else {
+                int correctIndex = arr[i] - 1;
+                if (arr[i] != arr[correctIndex]) {
+                    swap(arr, i, correctIndex);
+                } else {
+                    i++;
+                }
+            }
+        }
     }
 }
