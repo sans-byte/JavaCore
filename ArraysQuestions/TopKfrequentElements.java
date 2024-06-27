48ffed0b30d81f8d29c169bb1a760d0bd7834ad7
sans-byte <sanskarj15@gmail.com>
2024-06-27 18:31:01 +0530

    leetcode question

--- ArraysQuestions/TopKfrequentElements.java
+++ ArraysQuestions/TopKfrequentElements.java
@@ -1,30 +1,52 @@
 package ArraysQuestions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 // https://leetcode.com/problems/top-k-frequent-elements/
 public class TopKfrequentElements {
     public static void main(String[] args) {
-        int[] nums = { 1, 1, 1, 2, 2, 3,5,5,5,5,5,1,1,1,1,3,3,3,3,3,3,3,8,8,8,8,9 };
+        int[] nums = { 1, 1, 1, 2, 3, 5, 5, 5, 5, 5, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 8, 8, 8, 8, 9 };
         int k = 2;
 
-        topKfrequentElements(nums, k);
+        System.out.println(Arrays.toString(topKfrequentElements(nums, k)));
     }
 
     static int[] topKfrequentElements(int[] nums, int k) {
         Map<Integer, Integer> m = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (m.containsKey(nums[i])) {
-                int n = m.get(nums[i]);
-                m.put(nums[i], n + 1);
-            } else {
-                m.put(nums[i], 1);
+        for (int num : nums) {
+            m.put(num, m.getOrDefault(num, 0) + 1); // This will on every occurence of an element increase the count
+                                                    // with 1.
+        }
+        ArrayList<Integer>[] list = new ArrayList[nums.length + 1];
+
+        for (int key : m.keySet()) {
+            int freq = m.get(key);
+            if (list[freq] == null) {
+                list[freq] = new ArrayList<>();
             }
+            list[freq].add(key);
         }
 
-        System.out.println(m);
+        System.out.println(Arrays.toString(list));
 
-        return null;
+        int[] res = new int[k];
+
+        int tracker = 0;
+
+        for (int i = list.length - 1; i > 0; i--) {
+            if (list[i] == null)
+                continue;
+            for (int j = 0; j < list[i].size(); j++) {
+                res[tracker] = list[i].get(j);
+                tracker++;
+                if (tracker == k)
+                    return res;
+            }
+        }
+        
+        return res;
     }
 }
