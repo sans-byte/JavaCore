5e52b92b660882a72f40742d8bbe4ba4bcea429c
sans-byte <sanskarj15@gmail.com>
2024-07-16 15:16:49 +0530

    longest subarrray

---
+++ ArraysQuestions/LongestSubArrayWithKSum.java
@@ -0,0 +1,16 @@
+package ArraysQuestions;
+
+public class LongestSubArrayWithKSum {
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 5, 2, 7, 1, 9 };
+        int k = 15;
+
+        System.out.println(longestSubArray(arr));
+    }
+
+    static int longestSubArray(int[] arr) {
+        
+        return 0;
+    }
+}
