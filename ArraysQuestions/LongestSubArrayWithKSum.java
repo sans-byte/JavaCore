bdf9a3e981b9b277856d8fb5d9ac819ab470aad1
sans-byte <sanskarj15@gmail.com>
2024-07-17 17:33:47 +0530

    longest sub array

--- ArraysQuestions/LongestSubArrayWithKSum.java
+++ ArraysQuestions/LongestSubArrayWithKSum.java
@@ -1,16 +1,33 @@
 package ArraysQuestions;
 
+import java.util.HashMap;
+
 public class LongestSubArrayWithKSum {
 
     public static void main(String[] args) {
-        int[] arr = { 10, 5, 2, 7, 1, 9 };
-        int k = 15;
+        int[] arr = { 1, 2, 3 };
+        int k = 3;
 
-        System.out.println(longestSubArray(arr));
+        System.out.println(longestSubArray(arr, k));
     }
 
-    static int longestSubArray(int[] arr) {
+    static int longestSubArray(int[] arr, int k) {
+
+        HashMap<Integer, Integer> sumF = new HashMap<>();
+        sumF.put(0, 1);
         
-        return 0;
+        int cSum = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            cSum += num;
+            
+            if (sumF.containsKey(cSum - k)) {
+                count += sumF.get(cSum - k);
+            }
+            sumF.put(cSum, sumF.getOrDefault(cSum, 0) + 1);
+        }
+        
+        return count;
     }
 }
