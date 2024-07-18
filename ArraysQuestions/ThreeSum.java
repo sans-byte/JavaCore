1c90d3e8e0e402fafed293ac44fd9b9c9f7de9a1
sans-byte <sanskarj15@gmail.com>
2024-07-18 23:36:25 +0530

    Three sum

---
+++ ArraysQuestions/ThreeSum.java
@@ -0,0 +1,10 @@
+package ArraysQuestions;
+
+// https://leetcode.com/problems/3sum/description/
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        
+    }
+}
