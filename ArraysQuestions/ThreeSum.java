7996eaeb8cabb82fe9198fce2a3af68df5863e37
sans-byte <sanskarj15@gmail.com>
2024-07-18 23:52:42 +0530

    My solution on three sum problem

--- ArraysQuestions/ThreeSum.java
+++ ArraysQuestions/ThreeSum.java
@@ -2,12 +2,33 @@ package ArraysQuestions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 
 // https://leetcode.com/problems/3sum/description/
 
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
+    }
+
+    static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> res = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0;i<nums.length;i++){
+            List<Integer> twoSums = twoSum(nums,i,nums[i]);
+            if(twoSums.size() > 0){
+                twoSums.add(nums[i]);
+                twoSums.sort(null);
+                res.add(twoSums);
+            }
+        }
+        for(List<Integer> i : res){
+            result.add(i);
+        }
+        return result;
     }
 
     // Two sum on a sorted array
