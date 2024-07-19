1ea6553524539b81069909a82e68cae727ffe329
sans-byte <sanskarj15@gmail.com>
2024-07-19 18:38:11 +0530

    three sum complete

--- ArraysQuestions/ThreeSum.java
+++ ArraysQuestions/ThreeSum.java
@@ -5,14 +5,20 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 // https://leetcode.com/problems/3sum/description/
 
 public class ThreeSum {
     public static void main(String[] args) {
         int[] nums = { -1, 0, 1, 2, -1, -4 };
-        System.out.println(threeSum(nums));
+        System.out.println(threeSumOptimised(nums));
     }
+
+    // Intution here is to pick one one element from the array and apply 2sum on the
+    // remaining array
+    // Which will give you all the 3sums
+    // Below is easy to underatand function wise implementation of it
 
     static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
@@ -52,5 +58,38 @@ public class ThreeSum {
                 i++;
         }
         return res;
+    }
+    // Here I'll try to write more optimised code
+
+    static List<List<Integer>> threeSumOptimised(int[] arr) {
+        Arrays.sort(arr);
+        Set<List<Integer>> res = new LinkedHashSet<>();
+        int k = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = k - 1;
+
+            while (l < r) {
+                int target = arr[i] + arr[r] + arr[l];
+                if (target == 0) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[l]);
+                    temp.add(arr[r]);
+                    res.add(temp);
+                    l++;
+                    r--;
+                } else if (target > 0) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>(res);
+        return result;
     }
 }
