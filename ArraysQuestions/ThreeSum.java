24bb153c2098e0a619fdb66a1069dc4d3bbb083f
sans-byte <sanskarj15@gmail.com>
2024-07-19 18:20:54 +0530

    My solution for three sum

--- ArraysQuestions/ThreeSum.java
+++ ArraysQuestions/ThreeSum.java
@@ -10,44 +10,46 @@ import java.util.HashSet;
 
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        System.out.println(threeSum(nums));
     }
 
     static List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         Set<List<Integer>> res = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
-        for(int i = 0;i<nums.length;i++){
-            List<Integer> twoSums = twoSum(nums,i,nums[i]);
-            if(twoSums.size() > 0){
-                twoSums.add(nums[i]);
-                twoSums.sort(null);
-                res.add(twoSums);
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> twoSums = twoSum(nums, i + 1, nums[i]);
+            if (twoSums.size() > 0) {
+                for (List<Integer> sums : twoSums) {
+                    sums.add(0, nums[i]);
+                    res.add(sums);
+                }
             }
         }
-        for(List<Integer> i : res){
+        for (List<Integer> i : res) {
             result.add(i);
         }
         return result;
     }
 
-    // Two sum on a sorted array
-    static List<Integer> twoSum(int[] numbers,int start,int sum){
-        int i = 0;
+    static List<List<Integer>> twoSum(int[] numbers, int start, int sum) {
+        int i = start;
         int j = numbers.length - 1;
-        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         int targetSum = 0;
-        while(i<j){
-            if(i == start) i++;
-            if(j == start) j--;
+        while (i < j) {
             targetSum = numbers[i] + numbers[j] + sum;
-            if(targetSum == 0){
-                res.add(numbers[i]);
-                res.add(numbers[j]);
-                return res;
+            if (targetSum == 0) {
+                List<Integer> result = new ArrayList<>();
+                result.add(numbers[i]);
+                result.add(numbers[j]);
+                res.add(result);
             }
-            if(targetSum > 0) j--;
-            else i++;
+            if (targetSum > 0)
+                j--;
+            else
+                i++;
         }
         return res;
     }
