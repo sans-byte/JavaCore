ca2952db4855c5d8d050e7a7f819bf4c83f8e2a9
sans-byte <sanskarj15@gmail.com>
2024-07-28 15:51:54 +0530

    Minimum number of platfroms

--- GreedyAlgo/MinimumPlatforms.java
+++ GreedyAlgo/MinimumPlatforms.java
@@ -13,29 +13,55 @@ public class MinimumPlatforms {
     }
 
     static int minPlatforms(int[] arr, int[] dep) {
-        int[][] combined = new int[arr.length][2];
-        for (int x = 0; x < arr.length; x++) {
-            combined[x] = new int[] { arr[x], dep[x] };
+        // My code
+        // int[][] combined = new int[arr.length][2];
+        // for (int x = 0; x < arr.length; x++) {
+        // combined[x] = new int[] { arr[x], dep[x] };
+        // }
+
+        // Arrays.sort(combined, (a, b) -> a[0] - b[0]);
+        // List<Integer> list = new ArrayList<>();
+        // int i = 1;
+        // list.add(combined[0][1]);
+        // while (i < combined.length) {
+        // int flag = 0;
+        // for (int x = 0; x < list.size(); x++) {
+        // if (list.get(x) < combined[i][0]) {
+        // list.set(x, combined[i][1]);
+        // flag = 1;
+        // break;
+        // }
+        // }
+        // if (flag == 0) {
+        // list.add(combined[i][1]);
+        // }
+        // i++;
+        // }
+        // return list.size();
+
+        // Actual logic
+        // Intution here is to just focus on arrival and departure
+
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+
+        int count = 0;
+        int maxcount = 0;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < arr.length && j < dep.length) {
+            if (arr[i] < dep[j]) {
+                count++;
+                i = i + 1;
+            } else {
+                count--;
+                j = j + 1;
+            }
+            maxcount = Math.max(maxcount, count);
         }
 
-        Arrays.sort(combined, (a, b) -> a[0] - b[0]);
-        List<Integer> list = new ArrayList<>();
-        int i = 1;
-        list.add(combined[0][1]);
-        while (i < combined.length) {
-            int flag = 0;
-            for (int x = 0; x < list.size(); x++) {
-                if (list.get(x) < combined[i][0]) {
-                    list.set(x, combined[i][1]);
-                    flag = 1;
-                    break;
-                }
-            }
-            if (flag == 0) {
-                list.add(combined[i][1]);
-            }
-            i++;
-        }
-        return list.size();
+        return maxcount;
     }
 }
