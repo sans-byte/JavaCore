a65b424dfcfc07bbbb3503ea0ea32ffee12b5898
sans-byte <sanskarj15@gmail.com>
2024-07-29 18:17:58 +0530

    coin chnage

---
+++ GreedyAlgo/CoinChange.java
@@ -0,0 +1,13 @@
+package GreedyAlgo;
+
+public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins = { 186, 419, 83, 408 };
+        int amount = 6249;
+        System.out.println(needCoins(coins, amount));
+    }
+
+    static int needCoins(int[] arr, int amount) {
+        return 0;
+    }
+}
