0472446e8bf590deab4ebed02ccc90265801d31d
sanskar jain <sanskarj15@gmail.com>
2024-10-16 22:15:12 +0530

    heap and priority queue concept

---
+++ Heap/Heap.java
@@ -0,0 +1,27 @@
+package Heap;
+
+
+/*
+ * Array repersentation of BT
+ * Complete Binary Tree - The height will always be logN
+ * Heap - 
+ *       Max Heap - 1. Heap is a cpmplete binary  tree.
+ *                  2. Every parent will have the value greater then or equal to all its decendents
+ * 
+ *      Min Heap -  1. Heap is a complete binary tree.
+ *                  2. Every parent will be heaving the value lesser or equal to all its decendents
+ *           
+ * Insert and Delete -
+ *          Insert - time - worst case logN (height of the tree)
+ *          Deletion - 1. MaxHeap - Only max element that is only root element is removed or deleted and vise versa
+ *               2. time - worst case logN   
+ * Heap Sort - when we delete elements fron heap we get the largest element that largest element we can put at the empty place we got at the last of the heap (in array representation) if we keep doing it we will get the array sorted at last which is the concept of heap sort : -
+ *  - 1. Create a heap
+ *  - 2. Delete all the elements one by one from the heap.
+ * Heapify - 
+ * Priority Queues
+ */
+
+public class Heap {
+    
+}
