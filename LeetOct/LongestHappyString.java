c7872ac92943abb0855af4f28bc9a87cc6a48e88
sanskar jain <sanskarj15@gmail.com>
2024-10-16 22:15:23 +0530

    longest Happy string

---
+++ LeetOct/LongestHappyString.java
@@ -0,0 +1,14 @@
+package LeetOct;
+
+public class LongestHappyString {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int c = 7;
+        System.out.println(longestDiverseString(a, b, c));
+    }
+
+    public static String longestDiverseString(int a, int b, int c) {
+        return "aabbcc";
+    }
+}
