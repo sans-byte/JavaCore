8f3b765be57a6f38c090b2db78e4db9828d234dd
sanskar jain <sanskarj15@gmail.com>
2024-10-23 11:53:00 +0530

    Example question

--- Tree/Question.java
+++ Tree/Question.java
@@ -5,5 +5,6 @@ public class Question {
         int arr[] = { 1, 2, 3, 4, 5, 6 };
         TreeNode n = TreeNode.createTree(arr, 0);
         System.out.println(TreeNode.levelOrder(n));
+        TreeNode.postOrder(n);
     }
 }
