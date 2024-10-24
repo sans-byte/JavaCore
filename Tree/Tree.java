0c9aeb4b4fe0075483aa948560a9243d7b542503
sanskar jain <sanskarj15@gmail.com>
2024-10-24 22:53:08 +0530

    helper

---
+++ Tree/Tree.java
@@ -0,0 +1,73 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Tree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode createTree(int[] vals, int i) {
+        if (i >= vals.length)
+            return null;
+
+        TreeNode root = new TreeNode(vals[i]);
+        root.left = createTree(vals, 2 * i + 1);
+        root.right = createTree(vals, 2 * i + 2);
+        return root;
+    }
+
+    public static void postOrder(TreeNode n) {
+        if (n == null)
+            return;
+
+        System.out.println(n.val);
+        postOrder(n.left);
+        postOrder(n.right);
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode n) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (n == null)
+            return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(n);
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            List<Integer> temp = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode tempNode = q.poll();
+                if (tempNode.right != null)
+                    q.add(tempNode.right);
+
+                if (tempNode.left != null)
+                    q.add(tempNode.left);
+
+                temp.add(tempNode.val);
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+
+}
