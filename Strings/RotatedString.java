a3eb1071e0d70ff209e6c45f7f2edf0f81a79517
sans-byte <sanskarj15@gmail.com>
2024-11-04 16:10:56 +0530

    rotated string

---
+++ Strings/RotatedString.java
@@ -0,0 +1,13 @@
+package Strings;
+
+import java.util.*;
+
+public class RotatedString {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println((s + s).contains(goal)); //this is with concat strings
+
+        
+    }
+}
