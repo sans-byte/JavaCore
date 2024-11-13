d4be1b4d1069d3b5114793f0daaeff6118ec6c28
sanskar jain <sanskarj15@gmail.com>
2024-11-13 13:14:08 +0530

    count the number of fair pairs

--- LeetOct/CountTheNumberOfFairPairs.java
+++ LeetOct/CountTheNumberOfFairPairs.java
@@ -5,22 +5,32 @@ import java.util.*;
 
 public class CountTheNumberOfFairPairs {
     public static void main(String[] args) {
-        int nums[] = { -5,-7,-5,-7,-5 };
-        int lower = -12;
-        int upper = -12;
+        int nums[] = { 0, 1, 7, 4, 4, 5 };
+        int lower = 3;
+        int upper = 6;
         System.out.println(countFairPairs(nums, lower, upper));
     }
 
-    public static long countFairPairs(int[] nums, int lower, int upper) {
-        long count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[i] + nums[j] >= lower && nums[i] + nums[j] <= upper) {
-                    count++;
-                }
+    // The intution here is to find all the pairs which sums to more than lower
+    // Then find all the pairs which sums to more than upper
+    // And substract both
+    // We can find the pairs with 2 pointers approach
+
+    public static long findPairsMoreThanSum(int[] nums, int sum) {
+        int res = 0, i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] > sum)
+                j--;
+            else {
+                res += j - i;
+                i++;
             }
         }
-        System.out.println(count);
-        return 0;
+        return res;
+    }
+
+    public static long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return findPairsMoreThanSum(nums, upper) - findPairsMoreThanSum(nums, lower - 1);
     }
 }
