a6a141c1576d2322337af162638af9b208fe3fea
sans-byte <sanskarj15@gmail.com>
2024-11-14 23:32:40 +0530

    leetcode binary

---
+++ BinarySearch/CapacityToShitPackagesInDDays.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class CapacityToShitPackagesInDDays {
+    public static void main(String[] args) {
+        int weights[] = { 3, 2, 2, 4, 1, 4 }, days = 3;
+        shipWithinDays(weights, days);
+    }
+
+    public static int shipWithinDays(int[] weights, int days) {
+        int s = 0;
+        int e = 0;
+        for (int i : weights) {
+            e += i;
+            s = Math.max(s, i);
+        }
+
+        while (s <= e) {
+            int dayCount = 1;
+            int tempWeight = 0;
+            int mid = s + (e - s) / 2;
+            for (int i = 0; i < weights.length; i++) {
+                tempWeight += weights[i];
+                if (tempWeight > mid) {
+                    dayCount++;
+                    tempWeight = weights[i];
+                }
+            }
+
+            if (dayCount > days) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        System.out.println(s);
+        return 0;
+    }
+}
