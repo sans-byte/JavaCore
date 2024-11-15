ea3b582c0b8150d3ee3fa353a947bf44cb4eff1b
sanskar jain <sanskarj15@gmail.com>
2024-11-15 16:52:10 +0530

    complete solution

--- LeetOct/ShortestSubArrayToBeRemovedToMakeTheArraySorted.java
+++ LeetOct/ShortestSubArrayToBeRemovedToMakeTheArraySorted.java
@@ -6,25 +6,34 @@ import java.util.*;
 
 public class ShortestSubArrayToBeRemovedToMakeTheArraySorted {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 10, 4, 2, 3, 5 };
+        // int arr[] = { 1, 2, 3, 10, 4, 2, 3, 5 };
+        int arr[] = { 1, 2, 3, 10, 0, 7, 8, 9 };
         System.out.println(findLengthOfShortestSubarray(arr));
     }
 
     public static int findLengthOfShortestSubarray(int[] arr) {
-        int i = 0, j = arr.length - 1, maxCount = 1;
-        while (i < arr.length - 2 && arr[i + 1] >= arr[i]) {
+        int i = 0, j = arr.length - 1, maxCount = 0;
+        while (i < arr.length - 1 && arr[i + 1] >= arr[i]) {
             i++;
         }
-        while (j >= 1 && arr[j - 1] <= arr[j]) {
+        if (i == arr.length - 1)
+            return 0;
+
+        while (j > 0 && arr[j - 1] <= arr[j]) {
             j--;
         }
-        maxCount = Math.max(i + 1, arr.length - j);
-        while (i >= 0 && (i == j || arr[i] > arr[j])) {
-            i--;
-        }
 
-        int val = i + 1 + arr.length - j;
-        maxCount = Math.max(maxCount, val);
+        maxCount = Math.max(i + 1, arr.length - j);
+        int left = 0;
+        int right = j;
+        while (left <= i && right < arr.length) {
+            if (arr[left] <= arr[right]) {
+                maxCount = Math.max(maxCount, arr.length - right + left + 1);
+                left++;
+            } else {
+                right++;
+            }
+        }
         return arr.length - maxCount;
     }
 }
