e8fcce831d52382fbf1644dc36ca73b75f3e6987
sans-byte <sanskarj15@gmail.com>
2024-11-17 22:50:22 +0530

    start

---
+++ LeetOct/ShortestSubArrayWithSumAtLeastK.java
@@ -0,0 +1,14 @@
+package LeetOct;
+
+public class ShortestSubArrayWithSumAtLeastK {
+    public static void main(String[] args) {
+        int nums[] = { -28, 81, -20, 28, -29 };
+        int k = 89;
+        System.out.println(shortestSubarray(nums, k));
+    }
+
+    public static int shortestSubarray(int[] nums, int k) {
+        
+        return 0;
+    }
+}
