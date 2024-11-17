ffe60e4cbcb880cb0c995cd0ab70ae664581357d
sans-byte <sanskarj15@gmail.com>
2024-11-18 01:45:03 +0530

    shortest subarray with sum at least k

--- LeetOct/ShortestSubArrayWithSumAtLeastK.java
+++ LeetOct/ShortestSubArrayWithSumAtLeastK.java
@@ -4,6 +4,8 @@ package LeetOct;
 
 // TODO: Revice again -> Monotonus deque concept
 
+import java.util.*;
+
 public class ShortestSubArrayWithSumAtLeastK {
     public static void main(String[] args) {
         int nums[] = { -28, 81, -20, 28, -29 };
@@ -12,7 +14,23 @@ public class ShortestSubArrayWithSumAtLeastK {
     }
 
     public static int shortestSubarray(int[] nums, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        int prefix[] = new int[nums.length + 1];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prefix.length - 1; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        for (int i = 0; i < prefix.length; i++) {
+            while (!deque.isEmpty() && prefix[i] - prefix[deque.peekFirst()] >= k) {
+                min = Math.min(min, i - deque.pollFirst());
+            }
 
-        return 0;
+            while (!deque.isEmpty() && prefix[i] <= prefix[deque.peekLast()]) {
+                deque.pollLast();
+            }
+
+            deque.addLast(i);
+        }
+        return min;
     }
 }
