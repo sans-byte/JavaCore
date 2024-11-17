019923868713c23724d5924e47b1bf09f8e7f377
sans-byte <sanskarj15@gmail.com>
2024-11-18 01:38:16 +0530

    concept

--- LeetOct/ShortestSubArrayWithSumAtLeastK.java
+++ LeetOct/ShortestSubArrayWithSumAtLeastK.java
@@ -1,5 +1,9 @@
 package LeetOct;
 
+// 862. Shortest Subarray with Sum at Least K
+
+// TODO: Revice again -> Monotonus deque concept
+
 public class ShortestSubArrayWithSumAtLeastK {
     public static void main(String[] args) {
         int nums[] = { -28, 81, -20, 28, -29 };
@@ -8,7 +12,7 @@ public class ShortestSubArrayWithSumAtLeastK {
     }
 
     public static int shortestSubarray(int[] nums, int k) {
-        
+
         return 0;
     }
 }
