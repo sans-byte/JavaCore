4fa3d54b94f6827f36de89316d100eb69683d8b4
sanskar jain <sanskarj15@gmail.com>
2024-11-23 20:08:42 +0530

    not dont

---
+++ GFG160/MinimizeTheHeights2.java
@@ -0,0 +1,21 @@
+package GFG160;
+
+import java.util.*;
+
+public class MinimizeTheHeights2 {
+    public static void main(String[] args) {
+        // int arr[] = { 3, 9, 12, 16, 20 };
+        // int arr[] = { 1, 5, 8, 10 };
+        int arr[] = { 1, 1, 1, 10, 3, 2, 6, 9, 9, 8 };
+        // int arr[] = { 1, 8, 10, 6, 4, 6, 9, 1 };
+        // int k = 3;
+        // int k = 2;
+        // int k = 7;
+        int k = 2;
+        System.out.println(getMinDiff(arr, k));
+    }
+
+    static int getMinDiff(int[] arr, int k) {
+        return 0;
+    }
+}
