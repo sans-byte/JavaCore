823081bfc5c0d90792ca8556d74d45fad3a55a2f
sanskar jain <sanskarj15@gmail.com>
2024-11-25 00:31:24 +0530

    MaximumMatrixSum

--- LeetOct/MaximumMatirixSum.java
+++ LeetOct/MaximumMatirixSum.java
@@ -4,17 +4,24 @@ import java.util.*;
 
 public class MaximumMatirixSum {
     public static void main(String[] args) {
-        int matrix = { { 1, 2, 3 }, { -1, -2, -3 }, { 1, 2, 3 } };
+        int matrix[][] = { { 1, 2, 3 }, { -1, -2, -3 }, { 1, 2, 3 } };
         System.out.println(maxMatrixSum(matrix));
     }
 
     public static long maxMatrixSum(int[][] matrix) {
-        Set<Integer> s = new HashSet<>();
+        int negCount = 0, min = Integer.MAX_VALUE, sum = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                if()
+                if (matrix[i][j] < 0) {
+                    negCount++;
+                }
+                int val = Math.abs(matrix[i][j]);
+                min = Math.min(val, min);
+                sum += val;
             }
         }
-        return 0;
+        if (negCount % 2 == 0)
+            return sum;
+        return sum - 2 * min;
     }
 }
