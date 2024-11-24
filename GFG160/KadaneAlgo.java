3bd07f33b761f72277cc2925bdf16940c1a79cf7
sanskar jain <sanskarj15@gmail.com>
2024-11-24 22:56:18 +0530

    kadane's algo

---
+++ GFG160/KadaneAlgo.java
@@ -0,0 +1,24 @@
+package GFG160;
+
+//Kadane's Algorithm
+// Maximum sum subarray
+
+public class KadaneAlgo {
+    public static void main(String[] args) {
+        int arr[] = { 2, 3, -8, 7, -1, 2, 3 };
+        System.out.println(maxSubarraySum(arr));
+    }
+
+    static int maxSubarraySum(int[] arr) {
+        int maxSum = arr[0], sum = maxSum, i = 1;
+        while (i < arr.length) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += arr[i];
+            maxSum = Math.max(maxSum, sum);
+            i++;
+        }
+        return maxSum;
+    }
+}
