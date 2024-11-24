ab5ec928ec434bef8f0eb9d76b991e461b8712a3
sanskar jain <sanskarj15@gmail.com>
2024-11-24 23:26:13 +0530

    commit

---
+++ LeetOct/MaximumMatirixSum.java
@@ -0,0 +1,20 @@
+package LeetOct;
+
+import java.util.*;
+
+public class MaximumMatirixSum {
+    public static void main(String[] args) {
+        int matrix = { { 1, 2, 3 }, { -1, -2, -3 }, { 1, 2, 3 } };
+        System.out.println(maxMatrixSum(matrix));
+    }
+
+    public static long maxMatrixSum(int[][] matrix) {
+        Set<Integer> s = new HashSet<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if()
+            }
+        }
+        return 0;
+    }
+}
