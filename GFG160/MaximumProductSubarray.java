ad68b06b72f37cb30195d4024afa8ecfa3e7c148
sanskar jain <sanskarj15@gmail.com>
2024-11-25 22:16:14 +0530

    need to do again

--- GFG160/MaximumProductSubarray.java
+++ GFG160/MaximumProductSubarray.java
@@ -1,5 +1,7 @@
 package GFG160;
 
+// TODO : Complete this
+
 public class MaximumProductSubarray {
     public static void main(String[] args) {
         int arr[] = { -2, 6, -3, -10, 0, 2 };
@@ -7,17 +9,6 @@ public class MaximumProductSubarray {
     }
 
     static int maxProduct(int[] arr) {
-        int i = 0, j = 1, product = arr[i], max = product;
-        while (i < arr.length && j < arr.length) {
-            product *= arr[j];
-            if (product < 0) {
-                if (arr[j] > 0) {
-
-                } else {
-
-                }
-            }
-        }
-        return max;
+        return 0;
     }
 }
