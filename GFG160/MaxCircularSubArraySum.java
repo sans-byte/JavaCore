46353c37d880ed852caa5c93a9d2964678c3f99d
sanskar jain <sanskarj15@gmail.com>
2024-11-27 01:08:07 +0530

    tle solution

--- GFG160/MaxCircularSubArraySum.java
+++ GFG160/MaxCircularSubArraySum.java
@@ -1,13 +1,31 @@
 package GFG160;
 
+import java.util.*;
+
 class MaxCircularSubArraySum {
     public static void main(String[] args) {
-        int arr[] = { 10, -3, -4, 7, 6, 5, -4, -1 };
+        // int arr[] = { 10, -3, -4, 7, 6, 5, -4, -1 };
+        // int arr[] = { 8, -8, 9, -9, 10, -11, 12 };
+        int arr[] = { -311, 320 };
         System.out.println(circularSubarraySum(arr));
     }
 
     public static int circularSubarraySum(int arr[]) {
-        
-        return 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                int sum = arr[i];
+                int x = ++i;
+                x = x % arr.length;
+                for (int p = 1; p < arr.length; p++) {
+                    maxSum = Math.max(sum, maxSum);
+                    sum += arr[x];
+                    x++;
+                    x = x % arr.length;
+                }
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
     }
 }
