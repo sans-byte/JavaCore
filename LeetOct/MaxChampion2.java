2c91efcbe49332367c0a5f4ebdf8035633a47c43
sanskar jain <sanskarj15@gmail.com>
2024-11-26 23:43:36 +0530

    new approach

--- LeetOct/MaxChampion2.java
+++ LeetOct/MaxChampion2.java
@@ -7,6 +7,7 @@ public class MaxChampion2 {
     public static void main(String[] args) {
         int n = 3, edges[][] = { { 0, 1 }, { 1, 2 } };
         System.out.println(findChampion(n, edges));
+        System.out.println(findChampionWithArray(n, edges));
     }
 
     public static int findChampion(int n, int[][] edges) {
@@ -19,6 +20,23 @@ public class MaxChampion2 {
         int res = ((n - 1) * (n)) / 2;
         for (int i : s) {
             res -= i;
+        }
+        return res;
+    }
+
+    public static int findChampionWithArray(int n, int[][] edges) {
+        int temp[] = new int[n];
+        for (int[] row : edges) {
+            temp[row[1] - 1]++;
+        }
+        int res = -1;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] == 0 && res != -1) {
+                return -1;
+            }
+            if (temp[i] == 0) {
+                res = temp[i];
+            }
         }
         return res;
     }
