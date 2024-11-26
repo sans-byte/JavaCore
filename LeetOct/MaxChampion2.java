507fc804830ca6d29253113e65ebd5a2a1f8c1fd
sanskar jain <sanskarj15@gmail.com>
2024-11-26 21:49:04 +0530

    Max champion 2

---
+++ LeetOct/MaxChampion2.java
@@ -0,0 +1,26 @@
+package LeetOct;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MaxChampion2 {
+    public static void main(String[] args) {
+        int n = 3, edges[][] = { { 0, 1 }, { 1, 2 } };
+        System.out.println(findChampion(n, edges));
+    }
+
+    public static int findChampion(int n, int[][] edges) {
+        Set<Integer> s = new HashSet<>();
+        for (int[] row : edges) {
+            s.add(row[1]);
+        }
+        if (s.size() < n - 1)
+            return -1;
+        int res = ((n - 1) * (n)) / 2;
+        for (int i : s) {
+            res -= i;
+        }
+        return res;
+    }
+
+}
