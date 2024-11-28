42bfa6574a7c77b35588c4460f15747b1a5424f1
sanskar jain <sanskarj15@gmail.com>
2024-11-28 13:59:39 +0530

    Commit

--- GFG160/SmallestPositiveMissingNumber.java
+++ GFG160/SmallestPositiveMissingNumber.java
@@ -5,8 +5,11 @@ import java.util.Set;
 
 public class SmallestPositiveMissingNumber {
     public static void main(String[] args) {
-        int arr[] = { 2, -3, 4, 1, 1, 7 };
+        // int arr[] = { 2, -3, 4, 1, 1, 7 };
+        // int arr[] = { 100000, 3, 4000, 2, 15, 1, 99999 };
+        int arr[] = { 3, 4, -1, 1 };
         System.out.println(missingNumber(arr));
+        System.out.println(missingNumberBetter(arr));
     }
 
     public static int missingNumber(int[] arr) {
@@ -25,5 +28,33 @@ public class SmallestPositiveMissingNumber {
             i++;
         }
         return res + 1;
+    }
+
+    public static int missingNumberBetter(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i] = 0;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0 && Math.abs(arr[i]) <= arr.length) {
+                if (arr[Math.abs(arr[i]) - 1] == 0) {
+                    continue;
+                } else if (arr[Math.abs(arr[i]) - 1] <= 0) {
+                    continue;
+                } else {
+                    arr[Math.abs(arr[i]) - 1] *= -1;
+                }
+            }
+        }
+        int res = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                return res;
+            }
+            res++;
+        }
+        return res;
     }
 }
