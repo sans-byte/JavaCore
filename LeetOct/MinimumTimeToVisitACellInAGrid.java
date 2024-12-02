0d6d4090c56a8cd799e7a248afbd7ed940cadecd
sanskar jain <sanskarj15@gmail.com>
2024-12-02 12:59:40 +0530

    Refrence

--- LeetOct/MinimumTimeToVisitACellInAGrid.java
+++ LeetOct/MinimumTimeToVisitACellInAGrid.java
@@ -1,8 +1,10 @@
 package LeetOct;
 
+// 2577. Minimum Time to Visit a Cell In a Grid
+
 public class MinimumTimeToVisitACellInAGrid {
     public static void main(String[] args) {
-        
+
     }
 
     public static int minimumTime(int[][] grid) {
