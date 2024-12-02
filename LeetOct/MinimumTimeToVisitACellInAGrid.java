419d2dead52edb227418e9dd6ba21f073f6b608f
sanskar jain <sanskarj15@gmail.com>
2024-12-02 13:44:18 +0530

    need to fill in the gaps

--- LeetOct/MinimumTimeToVisitACellInAGrid.java
+++ LeetOct/MinimumTimeToVisitACellInAGrid.java
@@ -10,7 +10,9 @@ public class MinimumTimeToVisitACellInAGrid {
     }
 
     public static int minimumTime(int[][] grid) {
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x,y) -> x.get(2) - y.get(2));
+        if(grid[0][1] > 1 && grid[1][0] > 1) return -1;
+        
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((x, y) -> x.get(2) - y.get(2));
         List<Integer> temp = new LinkedList<>() {
             {
                 add(0); // x axis
@@ -22,11 +24,34 @@ public class MinimumTimeToVisitACellInAGrid {
         int m = grid.length;
         int n = grid[0].length;
 
-        int time[] = new int[m * n];
-        time[0] = 0;
+        int timeToReach[] = new int[m * n];
+        timeToReach[0] = 0;
 
         while (!pq.isEmpty()) {
-            
+            List<Integer> val = pq.poll();
+            int xAxis = val.get(0);
+            int yAxis = val.get(1);
+            int time = val.get(2);
+
+            if (xAxis - 1 >= 0) {
+                int tempxAxis = xAxis - 1;
+                int tempyAxis = yAxis;
+            }
+
+            if (xAxis + 1 < m) {
+                int tempxAxis = xAxis + 1;
+                int tempyAxis = yAxis;
+            }
+
+            if (yAxis - 1 >= 0) {
+                int tempxAxis = xAxis;
+                int tempyAxis = yAxis - 1;
+            }
+
+            if (yAxis + 1 < n) {
+                int tempxAxis = xAxis;
+                int tempyAxis = yAxis + 1;
+            }
         }
 
         return 0;
