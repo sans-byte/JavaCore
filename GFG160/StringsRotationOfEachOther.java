54f09cbab900f11ed8d21f69b48e7fdd9fbcb940
sanskar jain <sanskarj15@gmail.com>
2024-12-04 16:58:01 +0530

    using inbuild function

--- GFG160/StringsRotationOfEachOther.java
+++ GFG160/StringsRotationOfEachOther.java
@@ -7,14 +7,6 @@ public class StringsRotationOfEachOther {
     }
 
     public static boolean areRotations(String s1, String s2) {
-        if (s1.length() != s2.length())
-            return false;
-        StringBuilder s = new StringBuilder(s1);
-        for (int i = 0; i < s1.length(); i++) {
-            s.append(s.charAt(i));
-            if (s.substring(i+1).toString().equals(s2))
-                return true;
-        }
-        return false;
+        return (s1 + s1).contains(s2);
     }
 }
