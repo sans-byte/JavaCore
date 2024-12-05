c6b82ec3af7f9f6f6adc82a91999ff9e7fb3cd11
sanskar jain <sanskarj15@gmail.com>
2024-12-05 20:27:10 +0530

    move pieces to obtain a string

---
+++ LeetOct/MovePiecesToObtainAString.java
@@ -0,0 +1,48 @@
+package LeetOct;
+
+public class MovePiecesToObtainAString {
+    public static void main(String[] args) {
+        String start = "_L__R__R_", target = "L______RR";
+        // String start = "R_L_", target = "__LR";
+        System.out.println(canChange(start, target));
+    }
+
+    public static boolean canChange(String start, String target) {
+        int waitR = 0;
+        int waitL = 0;
+        for (int i = 0; i < start.length(); i++) {
+            char curr = start.charAt(i);
+            char goal = target.charAt(i);
+
+            if (curr == 'R') {
+                if (waitL > 0) {
+                    return false;
+                }
+                waitR++;
+            }
+
+            if (goal == 'R') {
+                if (waitR == 0) {
+                    return false;
+                }
+                waitR--;
+            }
+
+            if (goal == 'L') {
+                if (waitR > 0) {
+                    return false;
+                }
+                waitL++;
+            }
+
+            if (curr == 'L') {
+                if (waitL == 0) {
+                    return false;
+                }
+                waitL--;
+            }
+        }
+
+        return waitL == waitR && waitL == 0;
+    }
+}
