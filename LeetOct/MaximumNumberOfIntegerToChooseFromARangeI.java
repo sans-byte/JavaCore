c116ced1e5686483d5224a0e91aaad2285c30631
sanskar jain <sanskarj15@gmail.com>
2024-12-06 20:57:57 +0530

    leetcode question

---
+++ LeetOct/MaximumNumberOfIntegerToChooseFromARangeI.java
@@ -0,0 +1,29 @@
+package LeetOct;
+
+import java.util.*;
+
+public class MaximumNumberOfIntegerToChooseFromARangeI {
+    public static void main(String[] args) {
+        int banned[] = { 1, 6, 5 }, n = 5, maxSum = 6;
+        System.out.println(maxCount(banned, n, maxSum));
+    }
+
+    public static int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> s = new HashSet<>();
+        for (int i : banned)
+            s.add(i);
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!s.contains(i)) {
+                int temp = sum + i;
+                if (temp > maxSum) {
+                    return count;
+                }
+                sum = temp;
+                count++;
+            }
+        }
+        return count;
+    }
+}
