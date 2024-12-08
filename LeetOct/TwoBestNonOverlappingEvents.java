82bdf50d3fe64ad021b178e62d52cac2a550e179
sanskar jain <sanskarj15@gmail.com>
2024-12-08 20:18:00 +0530

    blueprint

---
+++ LeetOct/TwoBestNonOverlappingEvents.java
@@ -0,0 +1,12 @@
+package LeetOct;
+
+public class TwoBestNonOverlappingEvents {
+    public static void main(String[] args) {
+        int events[][] = { { 1, 3, 2 }, { 4, 5, 2 }, { 2, 4, 3 } };
+        System.out.println(maxTwoEvents(events));
+    }
+
+    public static int maxTwoEvents(int[][] events) {
+        return 0;
+    }
+}
