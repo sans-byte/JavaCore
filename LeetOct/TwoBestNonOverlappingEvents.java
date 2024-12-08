6b8d0379848f2d925dd9c89bd5acfb9ba7624e65
sanskar jain <sanskarj15@gmail.com>
2024-12-08 22:03:34 +0530

    complete code

--- LeetOct/TwoBestNonOverlappingEvents.java
+++ LeetOct/TwoBestNonOverlappingEvents.java
@@ -1,5 +1,11 @@
 package LeetOct;
 
+import java.lang.reflect.Array;
+
+// https://www.youtube.com/watch?v=Nhvf_hmDa6I
+
+import java.util.*;
+
 public class TwoBestNonOverlappingEvents {
     public static void main(String[] args) {
         int events[][] = { { 1, 3, 2 }, { 4, 5, 2 }, { 2, 4, 3 } };
@@ -7,6 +13,18 @@ public class TwoBestNonOverlappingEvents {
     }
 
     public static int maxTwoEvents(int[][] events) {
-        return 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> Integer.compare(x[1], y[1]));
+        Arrays.sort(events, (x, y) -> Integer.compare(x[0], y[0]));
+        int maxValue = 0, ans = 0;
+        for (int i = 0; i < events.length; i++) {
+            while (!pq.isEmpty() && pq.peek()[1] < events[i][0]) {
+                maxValue = Math.max(maxValue, pq.peek()[2]);
+                pq.poll();
+            }
+
+            ans = Math.max(ans, maxValue + events[i][2]);
+            pq.add(events[i]);
+        }
+        return ans;
     }
 }
