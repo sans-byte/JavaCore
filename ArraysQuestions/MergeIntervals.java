02ef3efbc86f35e859a4de19ec41985aa19fc051
sanskar jain <sanskarj15@gmail.com>
2024-12-09 13:27:24 +0530

    refactor

--- ArraysQuestions/MergeIntervals.java
+++ ArraysQuestions/MergeIntervals.java
@@ -1,9 +1,6 @@
 package ArraysQuestions;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MergeIntervals {
     public static void main(String[] args) {
