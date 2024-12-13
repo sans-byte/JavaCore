c0741ab67a75143965a9928ef02313fa65b0c114
sanskar jain <sanskarj15@gmail.com>
2024-12-13 15:27:55 +0530

    Sorted and rotated minimum

--- GFG160/Searching/SortedAndRotatedMinimum.java
+++ GFG160/Searching/SortedAndRotatedMinimum.java
@@ -2,23 +2,21 @@ package GFG160.Searching;
 
 public class SortedAndRotatedMinimum {
     public static void main(String[] args) {
-        int arr[] = { 1, 4, 3, 2 };
+        int arr[] = { 1, 2, 3, 4 };
         System.out.println(findMin(arr));
     }
 
     public static int findMin(int[] arr) {
         int l = 0;
         int r = arr.length - 1;
-        while (l <= r) {
+        while (l < r) {
             int mid = l + (r - l) / 2;
-            if (mid + 1 < arr.length && arr[mid + 1] < arr[mid]) {
-                l = mid + 1;
-            } else if (mid - 1 >= 0 && arr[mid - 1] < arr[mid]) {
-                r = mid - 1;
+            if (arr[mid] < arr[r]) {
+                r = mid;
             } else {
-                return arr[mid];
+                l = mid + 1;
             }
         }
-        return 0;
+        return arr[r];
     }
 }
