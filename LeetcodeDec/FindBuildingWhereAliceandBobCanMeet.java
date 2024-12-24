e506f4608ee20b96412827a2f88657d11ea49778
sanskar jain <sanskarj15@gmail.com>
2024-12-24 19:24:23 +0530

    BluePrint

--- LeetcodeDec/FindBuildingWhereAliceandBobCanMeet.java
+++ LeetcodeDec/FindBuildingWhereAliceandBobCanMeet.java
@@ -2,32 +2,21 @@ import java.util.*;
 
 public class FindBuildingWhereAliceandBobCanMeet {
     public static void main(String[] args) {
-        int heights[] = { 1, 2, 1, 2, 1, 2 }, queries[][] = { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 },
-                { 0, 5 },
-                { 1, 0 }, { 1, 1 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 0 }, { 2, 1 }, { 2, 2 }, { 2, 3 },
-                { 2, 4 }, { 2, 5 }, { 3, 0 }, { 3, 1 }, { 3, 2 }, { 3, 3 }, { 3, 4 }, { 3, 5 }, { 4, 0 }, { 4, 1 },
-                { 4, 2 }, { 4, 3 }, { 4, 4 }, { 4, 5 }, { 5, 0 }, { 5, 1 }, { 5, 2 }, { 5, 3 }, { 5, 4 }, { 5, 5 } };
+        // int heights[] = { 1, 2, 1, 2, 1, 2 }, queries[][] = { { 0, 0 }, { 0, 1 }, {
+        // 0, 2 }, { 0, 3 }, { 0, 4 },
+        // { 0, 5 },
+        // { 1, 0 }, { 1, 1 }, { 1, 2 }, { 1, 3 }, { 1, 4 }, { 1, 5 }, { 2, 0 }, { 2, 1
+        // }, { 2, 2 }, { 2, 3 },
+        // { 2, 4 }, { 2, 5 }, { 3, 0 }, { 3, 1 }, { 3, 2 }, { 3, 3 }, { 3, 4 }, { 3, 5
+        // }, { 4, 0 }, { 4, 1 },
+        // { 4, 2 }, { 4, 3 }, { 4, 4 }, { 4, 5 }, { 5, 0 }, { 5, 1 }, { 5, 2 }, { 5, 3
+        // }, { 5, 4 }, { 5, 5 } };
+
+        int heights[] = { 5, 3, 8, 2, 6, 1, 4, 6 }, queries[][] = { { 0, 7 }, { 3, 5 }, { 5, 2 }, { 3, 0 }, { 1, 6 } };
         System.out.println(Arrays.toString(leftmostBuildingQueries(heights, queries)));
     }
 
     public static int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        int res[] = new int[queries.length];
-        Arrays.fill(res, -1);
-
-        for (int i = 0; i < queries.length; i++) {
-            int alice = Math.min(queries[i][0], queries[i][1]);
-            int bob = Math.max(queries[i][0], queries[i][1]);
-            if (alice == bob) {
-                res[i] = alice;
-            } else {
-                for (int j = bob; j < heights.length; j++) {
-                    if (heights[j] >= heights[bob] && heights[j] > heights[alice]) {
-                        res[i] = j;
-                        break;
-                    }
-                }
-            }
-        }
-        return res;
+        
     }
 }
