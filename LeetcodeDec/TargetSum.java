31b3daffa55cccf8989b4a1593596bc11687dccb
sanskar jain <sanskarj15@gmail.com>
2024-12-26 23:51:52 +0530

    Target Sum

---
+++ LeetcodeDec/TargetSum.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class TargetSum {
+    public static void main(String[] args) {
+        int nums[] = { 1, 1, 1, 1, 1 }, target = 3;
+        System.out.println(findTargetSumWays(nums, target));
+    }
+
+    public static int helper(int nums[], int index, int sum, int target) {
+        if (index >= nums.length) {
+            if (target == sum) {
+                return 1;
+            }
+            return 0;
+        }
+        int add = helper(nums, index + 1, sum + nums[index], target);
+        int subs = helper(nums, index + 1, sum - nums[index], target);
+        return add + subs;
+    }
+
+    public static int findTargetSumWays(int[] nums, int target) {
+        return helper(nums, 0, 0, target);
+    }
+}
