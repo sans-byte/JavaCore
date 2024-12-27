c4e2d16ba419336d3aae9f9d5319e24a33d171a0
sanskar jain <sanskarj15@gmail.com>
2024-12-27 23:55:55 +0530

    Added code

--- LeetcodeDec/BestSightseeingPair.java
+++ LeetcodeDec/BestSightseeingPair.java
@@ -4,4 +4,14 @@ public class BestSightseeingPair {
     public static void main(String[] args) {
         
     }
+
+    public int maxScoreSightseeingPair(int[] values) {
+        int currMax = values[0] - 1;
+        int max = currMax;
+        for (int i = 1; i < values.length; i++) {
+            max = Math.max(max, values[i] + currMax);
+            currMax = Math.max(currMax-1, values[i] - 1);
+        }
+        return max;
+    }
 }
