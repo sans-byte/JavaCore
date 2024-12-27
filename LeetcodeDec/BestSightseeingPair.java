6ad78bb85736513736d77ea6c12b0c649785b8a2
sanskar jain <sanskarj15@gmail.com>
2024-12-27 23:57:12 +0530

    Complete Code

--- LeetcodeDec/BestSightseeingPair.java
+++ LeetcodeDec/BestSightseeingPair.java
@@ -2,15 +2,16 @@
 
 public class BestSightseeingPair {
     public static void main(String[] args) {
-        
+        int values[] = { 8, 1, 5, 2, 6 };
+        System.out.println(maxScoreSightseeingPair(values));
     }
 
-    public int maxScoreSightseeingPair(int[] values) {
+    public static int maxScoreSightseeingPair(int[] values) {
         int currMax = values[0] - 1;
         int max = currMax;
         for (int i = 1; i < values.length; i++) {
             max = Math.max(max, values[i] + currMax);
-            currMax = Math.max(currMax-1, values[i] - 1);
+            currMax = Math.max(currMax - 1, values[i] - 1);
         }
         return max;
     }
