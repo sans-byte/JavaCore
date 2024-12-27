ff9a69273f85dd5fd078858d737a67434e53ae54
sanskar jain <sanskarj15@gmail.com>
2024-12-27 23:52:51 +0530

    count pair with given sum

---
+++ GFG160/Hashing/CountPairWithGivenSum.java
@@ -0,0 +1,20 @@
+public class CountPairWithGivenSum{
+    public static void main(String args[]) {
+        int arr[] = {1, 5, 7, -1, 5}, target = 6; 
+    }
+
+    int countPairs(int arr[], int target) {
+        int count = 0;
+        Map<Integer, Integer> m = new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            int val = target - arr[i];
+            if(m.containsKey(val)){
+                count += m.get(val);
+            }
+            
+            m.put(arr[i],m.getOrDefault(arr[i] , 0) + 1);
+        }
+        return count;
+    }
+}
