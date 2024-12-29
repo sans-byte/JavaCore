b154ebfabb6e96793995a82d1a07e663061f39c6
sanskar jain <sanskarj15@gmail.com>
2024-12-29 15:01:02 +0530

    Number of Ways to Form a Target String Given a Dictionary

--- LeetcodeDec/Hard/NumberofWaystoFormaTargetStringGivenaDictionary.java
+++ LeetcodeDec/Hard/NumberofWaystoFormaTargetStringGivenaDictionary.java
@@ -2,10 +2,12 @@ package Hard;
 
 public class NumberofWaystoFormaTargetStringGivenaDictionary {
     public static void main(String[] args) {
-        
+        String words[] = { "acca", "bbbb", "caca" }, target = "aba";
+        System.out.println(numWays(words, target));
     }
 
-    public int numWays(String[] words, String target) {
+    public static int numWays(String[] words, String target) {
+        
         return 0;
     }
 }
