80e70169e0385b039c5dedf9e0b132ea0501ca33
sanskar jain <sanskarj15@gmail.com>
2024-12-29 14:58:22 +0530

    Find triplets

--- GFG160/Hashing/FindAllTriplitsWithZeroSum.java
+++ GFG160/Hashing/FindAllTriplitsWithZeroSum.java
@@ -10,7 +10,7 @@ public class FindAllTriplitsWithZeroSum {
 
     public static List<List<Integer>> findTriplets(int[] arr) {
         // This code is failing for 1 test case idk why
-        
+
         // List<List<Integer>> res = new ArrayList<>();
         // for (int i = 0; i < arr.length - 2; i++) {
         // int target = 0 - arr[i];
@@ -31,6 +31,37 @@ public class FindAllTriplitsWithZeroSum {
 
         // }
 
-        return res;
+        // return res;
+
+        Set<List<Integer>> set = new HashSet<>();
+        Map<Integer, List<int[]>> map = new HashMap<>(); // arr[a] + arr[b] = -arr[c] // we will store all a and b here
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                map.computeIfAbsent(arr[i] + arr[j], k -> new ArrayList<>()).add(new int[] { i, j });
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int value = -arr[i];
+            int index = i;
+            if (map.containsKey(value)) {
+                for (int[] pair : map.get(value)) {
+                    if (pair[0] != i && pair[1] != i) {
+                        List<Integer> temp = new ArrayList<>() {
+                            {
+                                add(pair[0]);
+                                add(pair[1]);
+                                add(index);
+                            }
+                        };
+
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(set);
     }
 }
