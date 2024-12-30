bffd1336047591bc181bbaee94588dddcfa36ba5
sanskar jain <sanskarj15@gmail.com>
2024-12-30 10:05:48 +0530

    Added blueprint

---
+++ LeetcodeDec/CountWaysToBuildGoodString.java
@@ -0,0 +1,9 @@
+public class CountWaysToBuildGoodString {
+    public static void main(String[] args) {
+        
+    }
+
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        return 0;
+    }
+}
