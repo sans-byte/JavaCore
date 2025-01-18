39813967a2b1f5e95448d4d0675164bcbfad7f1f
sanskar jain <sanskarj15@gmail.com>
2025-01-18 22:29:44 +0530

    blueprint

---
+++ LeetcodeJan/MinimumCostToMakeAtLeastOneValidPathInAGrid.java
@@ -0,0 +1,13 @@
+package LeetcodeJan;
+
+public class MinimumCostToMakeAtLeastOneValidPathInAGrid {
+    public static void main(String[] args) {
+        int grid[][] = { { 1, 1, 1, 1 }, { 2, 2, 2, 2 }, { 1, 1, 1, 1 }, { 2, 2, 2, 2 } };
+        System.out.println(minCost(grid));
+    }
+
+    public static int minCost(int[][] grid) {
+        int res = 0;
+        return 0;
+    }
+}
