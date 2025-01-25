2d959dcc4eadba3f08238da0fcd556efc86abea3
sanskar jain <sanskarj15@gmail.com>
2025-01-25 23:45:59 +0530

    commit

--- LeetcodeJan/MakeLexicographicallySmallestArraybySwappingElements.java
+++ LeetcodeJan/MakeLexicographicallySmallestArraybySwappingElements.java
@@ -1,5 +1,13 @@
 package LeetcodeJan;
 
 public class MakeLexicographicallySmallestArraybySwappingElements {
-    
+    public static void main(String[] args) {
+        int nums[] = { 1, 7, 6, 18, 2, 1 }, limit = 3;
+        System.out.println(lexicographicallySmallestArray(nums, limit));
+    }
+
+    public static int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int res[] = new int[nums.length];
+        return res;
+    }
 }
