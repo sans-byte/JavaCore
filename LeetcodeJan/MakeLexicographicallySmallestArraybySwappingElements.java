8b699a7e6f4d3f49f8de44c995dc03e09f48dd75
sanskar jain <sanskarj15@gmail.com>
2025-01-26 00:19:02 +0530

    commit

--- LeetcodeJan/MakeLexicographicallySmallestArraybySwappingElements.java
+++ LeetcodeJan/MakeLexicographicallySmallestArraybySwappingElements.java
@@ -1,24 +1,38 @@
 package LeetcodeJan;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MakeLexicographicallySmallestArraybySwappingElements {
     public static void main(String[] args) {
         int nums[] = { 1, 7, 6, 18, 2, 1 }, limit = 3;
-        System.out.println(lexicographicallySmallestArray(nums, limit));
+        System.out.println(Arrays.toString(lexicographicallySmallestArray(nums, limit)));
     }
 
     public static int[] lexicographicallySmallestArray(int[] nums, int limit) {
         int res[] = new int[nums.length];
         int temp[][] = new int[nums.length][2];
+        List<List<Integer>> l = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             temp[i] = new int[] { nums[i], i };
         }
-
         Arrays.sort(temp, (x, y) -> Integer.compare(x[0], y[0]));
-
-        for (int[] i : temp)
-            System.out.println(Arrays.toString(i));
+        l.add(new ArrayList<>(Arrays.asList(temp[0][1])));
+        for (int i = 1; i < temp.length; i++) {
+            if (temp[i][0] - temp[i - 1][0] <= limit)
+                l.get(l.size() - 1).add(temp[i][1]);
+            else {
+                l.add(new ArrayList<>(Arrays.asList(temp[i][1])));
+            }
+        }
+        for (int i = 0; i < l.size(); i++) {
+            Collections.sort(l.get(i));
+        }
+        int index = 0;
+        for (List<Integer> groups : l) {
+            for (int pos : groups) {
+                res[pos] = temp[index++][0];
+            }
+        }
         return res;
     }
 }
