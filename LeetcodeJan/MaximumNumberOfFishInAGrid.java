ba4673cc2caaf409a0ac172a1e9c28f7efac2d30
sanskar jain <sanskarj15@gmail.com>
2025-01-28 20:42:12 +0530

    blurprint

---
+++ LeetcodeJan/MaximumNumberOfFishInAGrid.java
@@ -0,0 +1,10 @@
+public class MaximumNumberOfFishInAGrid {
+    public static void main(String[] args) {
+        int grid[][] = { { 0, 2, 1, 0 }, { 4, 0, 0, 3 }, { 1, 0, 0, 4 }, { 0, 3, 2, 0 } };
+    }
+
+    public static int findMaxFish(int[][] grid) {
+        int res = 0;
+        return res;
+    }
+}
