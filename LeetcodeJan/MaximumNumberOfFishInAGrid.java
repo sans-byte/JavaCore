0386061252db6849caa9d15b66998d6a2f7926f0
sanskar jain <sanskarj15@gmail.com>
2025-01-28 20:45:15 +0530

    commit

--- LeetcodeJan/MaximumNumberOfFishInAGrid.java
+++ LeetcodeJan/MaximumNumberOfFishInAGrid.java
@@ -3,6 +3,9 @@ package LeetcodeJan;
 import java.util.*;
 
 public class MaximumNumberOfFishInAGrid {
+    public static int yAxis[] = { -1, 1, 0, 0 };
+    public static int xAxis[] = { 0, 0, -1, 1 };
+
     public static void main(String[] args) {
         int grid[][] = { { 0, 2, 1, 0 }, { 4, 0, 0, 3 }, { 1, 0, 0, 4 }, { 0, 3, 2, 0 } };
         System.out.println(findMaxFish(grid));
@@ -10,6 +13,7 @@ public class MaximumNumberOfFishInAGrid {
 
     public static int findMaxFish(int[][] grid) {
         int res = 0;
+
         return res;
     }
 }
