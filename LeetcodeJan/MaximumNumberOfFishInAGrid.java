9703f942bf1f04fb90a87f8d30ffc038ec83150f
sanskar jain <sanskarj15@gmail.com>
2025-01-28 21:06:02 +0530

    blurprint

--- LeetcodeJan/MaximumNumberOfFishInAGrid.java
+++ LeetcodeJan/MaximumNumberOfFishInAGrid.java
@@ -11,9 +11,37 @@ public class MaximumNumberOfFishInAGrid {
         System.out.println(findMaxFish(grid));
     }
 
+    // perform dfs and get the values
+    public static int helper(int[][] grid, int i, int j) {
+        if (grid[i][j] == 0) {
+            return 0;
+        }
+        int fishes = 0;
+        for (int k = 0; k < 4; k++) {
+            int row = i + yAxis[k];
+            int col = j + xAxis[k];
+            if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
+                fishes = grid[i][j] + helper(grid, row, col);
+            }
+        }
+        return fishes;
+    }
+
     public static int findMaxFish(int[][] grid) {
         int res = 0;
-
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    for (int k = 0; k < 4; k++) {
+                        int row = i + yAxis[k];
+                        int col = j + xAxis[k];
+                        if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
+                            res = Math.max(res, helper(grid, row, col));
+                        }
+                    }
+                }
+            }
+        }
         return res;
     }
 }
