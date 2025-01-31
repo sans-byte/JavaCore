a41443e4f93fbd798fdbfb678acce3ed2eaaf61d
sanskar jain <sanskarj15@gmail.com>
2025-01-31 15:52:58 +0530

    blueprint

---
+++ GFG160/RecursionAndBacktracking/SudokuSolver.java
@@ -0,0 +1,11 @@
+package GFG160.RecursionAndBacktracking;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        
+    }
+
+    static void solveSudoku(int[][] mat) {
+        // code here
+    }
+}
