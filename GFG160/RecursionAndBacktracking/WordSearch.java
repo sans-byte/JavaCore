c6892c38bd0b5d84bc7dc266ae7a3b52502b8858
sanskar jain <sanskarj15@gmail.com>
2025-02-01 20:54:34 +0530

    Blueprint

--- GFG160/RecursionAndBacktracking/WordSearch.java
+++ GFG160/RecursionAndBacktracking/WordSearch.java
@@ -1,3 +1,7 @@
 public class WordSearch {
-    
+    public static void main(String[] args) {
+        char mat[][] = { { 'T', 'E', 'E' }, { 'S', 'G', 'K' }, { 'T', 'E', 'L' } };
+        String word = "GEEK";
+    }
+
 }
