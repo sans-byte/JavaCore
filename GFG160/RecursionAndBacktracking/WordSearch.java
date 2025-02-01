3888470241a1f19cf2dd14b16d0a39b2800d9a8c
sanskar jain <sanskarj15@gmail.com>
2025-02-01 21:52:33 +0530

    Approach with bfs is not good coz this question needs backtracking

--- GFG160/RecursionAndBacktracking/WordSearch.java
+++ GFG160/RecursionAndBacktracking/WordSearch.java
@@ -1,3 +1,5 @@
+package GFG160.RecursionAndBacktracking;
+
 import java.util.*;
 
 public class WordSearch {
@@ -10,24 +12,26 @@ public class WordSearch {
         System.out.println(isWordExist(mat, word));
     }
 
-    static public boolean helper(char[][] mat, int row, int col, String word) {
+    static public boolean helper(char[][] mat, int row, int col, String word, int[][] visited) {
+        if (word.length() == 1)
+            return true;
         char wordArr[] = word.toCharArray();
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] { row, col });
-        int idx = 1;
+        q.add(new int[] { row, col, 1 });
+        visited[row][col] = 1;
         while (!q.isEmpty()) {
             for (int i = 0; i < q.size(); i++) {
                 int val[] = q.poll();
+                if (val[2] >= wordArr.length)
+                    return true;
                 for (int k = 0; k < 4; k++) {
                     int newRow = val[0] + yAxis[k];
                     int newCol = val[1] + xAxis[k];
 
                     if (newRow >= 0 && newRow < mat.length && newCol >= 0 && newCol < mat[0].length
-                            && mat[newRow][newCol] == wordArr[idx]) {
-                        q.add(new int[] { newRow, newCol });
-                        idx++;
-                        if (idx >= word.length())
-                            return true;
+                            && mat[newRow][newCol] == wordArr[val[2]] && visited[newRow][newCol] == 0) {
+                        q.add(new int[] { newRow, newCol, val[2] + 1 });
+                        visited[newRow][newCol] = 1;
                     }
                 }
             }
@@ -35,8 +39,17 @@ public class WordSearch {
         return false;
     }
 
-    static public boolean isWordExist(char[][] mat, String word) {
-
+    public static boolean isWordExist(char[][] mat, String word) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == word.charAt(0)) {
+                    int visited[][] = new int[mat.length][mat[0].length];
+                    if (helper(mat, i, j, word, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
-
 }
