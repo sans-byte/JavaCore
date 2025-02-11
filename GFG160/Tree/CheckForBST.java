1d00ea97c6c7014c9ff2d9c7c1236250c0590913
sanskar jain <sanskarj15@gmail.com>
2025-02-11 15:23:00 +0530

    Check for bst

--- GFG160/Tree/CheckForBST.java
+++ GFG160/Tree/CheckForBST.java
@@ -1,26 +1,16 @@
+import org.w3c.dom.Node;
+
 public class CheckForBST {
     public static void main(String[] args) {
         
     }
 
-    public static boolean helper(TreeNode root, int curr) {
-        if (root == null)
-            return true;
-        if (root.left != null && root.left.val >= curr)
-            return false;
-        if (root.right != null && root.right.val <= curr)
-            return false;
-
-        if (root.left != null && root.right != null) {
-            return helper(root.left, root.left.val) && helper(root.right, root.right.val);
-        } else if (root.left != null)
-            return helper(root.left, root.left.val);
-        else if (root.right != null)
-            return helper(root.right, root.right.val);
-        return true;
+    boolean helper(Node root, int min, int max){
+        if(root == null) return true;
+        if(root.data <= min || root.data >= max) return false;
+        return helper(root.left, min, root.data) && helper(root.right, root.data, max);
     }
-
-    public boolean isValidBST(TreeNode root) {
-        return helper(root, root.val);
+    boolean isBST(Node root) {
+        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 }
