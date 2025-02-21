ac36c5e29472a7bc26e2c0ece1f049dec9978182
sanskar jain <sanskarj15@gmail.com>
2025-02-21 22:53:36 +0530

    with stack method 1

--- LeetcodeFeb/FindElementsinaContaminatedBinaryTree.java
+++ LeetcodeFeb/FindElementsinaContaminatedBinaryTree.java
@@ -1,11 +1,16 @@
 package LeetcodeFeb;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindElementsinaContaminatedBinaryTree {
     public static void main(String[] args) {
         
     }
 
     class FindElements {
+        public static Set<Integer> set = new HashSet<>();
+
 
         public FindElements(TreeNode root) {
             
