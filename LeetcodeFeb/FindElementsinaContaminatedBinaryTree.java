28384cccd1b0f1dd081f98ec4bb3dc1f7c2d69c0
sanskar jain <sanskarj15@gmail.com>
2025-02-21 22:56:27 +0530

    Method 1 with set

--- LeetcodeFeb/FindElementsinaContaminatedBinaryTree.java
+++ LeetcodeFeb/FindElementsinaContaminatedBinaryTree.java
@@ -5,24 +5,25 @@ import java.util.Set;
 
 public class FindElementsinaContaminatedBinaryTree {
     public static void main(String[] args) {
-        
+
     }
 
     class FindElements {
         public static Set<Integer> set = new HashSet<>();
 
-        public static void generateValues(TreeNode root){
-            if(root == null) return;
-    
-            if(root.left != null) {
-                int value = 2*root.val + 1;
+        public static void generateValues(TreeNode root) {
+            if (root == null)
+                return;
+
+            if (root.left != null) {
+                int value = 2 * root.val + 1;
                 set.add(value);
                 root.left.val = value;
                 generateValues(root.left);
             }
-    
-            if(root.right != null) {
-                int value = 2*root.val + 2;
+
+            if (root.right != null) {
+                int value = 2 * root.val + 2;
                 set.add(value);
                 root.right.val = value;
                 generateValues(root.right);
@@ -30,11 +31,14 @@ public class FindElementsinaContaminatedBinaryTree {
         }
 
         public FindElements(TreeNode root) {
-            
+            set.clear();
+            root.val = 0;
+            set.add(0);
+            generateValues(root);
         }
-        
+
         public boolean find(int target) {
-            
+            return set.contains(target);
         }
     }
 }
