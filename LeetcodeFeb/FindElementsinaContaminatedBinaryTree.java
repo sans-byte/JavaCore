30f9556bd0502c0a5d9887243406f70c2b523739
sanskar jain <sanskarj15@gmail.com>
2025-02-21 22:39:19 +0530

    bluepring

---
+++ LeetcodeFeb/FindElementsinaContaminatedBinaryTree.java
@@ -0,0 +1,18 @@
+package LeetcodeFeb;
+
+public class FindElementsinaContaminatedBinaryTree {
+    public static void main(String[] args) {
+        
+    }
+
+    class FindElements {
+
+        public FindElements(TreeNode root) {
+            
+        }
+        
+        public boolean find(int target) {
+            
+        }
+    }
+}
