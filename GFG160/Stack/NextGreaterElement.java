a8a5a74256ee1e878b3bc01c516ce4122fe1aff6
sanskar jain <sanskarj15@gmail.com>
2025-02-23 23:18:45 +0530

    Next Greater Element

--- GFG160/Stack/NextGreaterElement.java
+++ GFG160/Stack/NextGreaterElement.java
@@ -7,6 +7,7 @@ import java.util.Stack;
 public class NextGreaterElement {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 2, 4 };
+        // int[] arr = { 50, 40, 30, 10;
         System.out.println(nextLargerElement(arr));
     }
 
