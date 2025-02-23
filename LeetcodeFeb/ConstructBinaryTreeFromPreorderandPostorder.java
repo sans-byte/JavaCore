b295613755b577207ebdc069641dab03be8d6189
sanskar jain <sanskarj15@gmail.com>
2025-02-23 22:32:37 +0530

    construct binary tree

---
+++ LeetcodeFeb/ConstructBinaryTreeFromPreorderandPostorder.java
@@ -0,0 +1,11 @@
+package LeetcodeFeb;
+
+public class ConstructBinaryTreeFromPreorderandPostorder {
+    public static void main(String[] args) {
+        
+    }
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return new TreeNode();
+    }
+}
