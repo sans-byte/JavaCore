2471554bfb32ba037a60770ae4d5c8628a91f5dc
sanskar jain <sanskarj15@gmail.com>
2025-02-24 23:46:29 +0530

    code that got accepted

--- GFG160/Stack/StackSpanProblem.java
+++ GFG160/Stack/StackSpanProblem.java
@@ -4,25 +4,49 @@ import java.util.*;
 
 public class StackSpanProblem {
     public static void main(String[] args) {
-        int arr[] = { 10, 4, 5, 90, 120, 80 };
-        System.out.println(calculateSpan(arr));
+        int arr[] = { 100, 80, 60, 70, 60, 75, 85 };
+        System.out.println(calculateSpanMap(arr));
     }
 
-    public static ArrayList<Integer> calculateSpan(int[] arr) {
-        ArrayList<Integer> res = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> temp = new Stack<>();
+    // public static ArrayList<Integer> calculateSpan(int[] arr) {
+    // ArrayList<Integer> res = new ArrayList<>();
+    // Stack<Integer> stack = new Stack<>();
+    // Stack<Integer> temp = new Stack<>();
 
-        for (int i = 0; i < arr.length; i++) {
+    // for (int i = 0; i < arr.length; i++) {
+    // int count = 1;
+    // while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+    // temp.add(stack.pop());
+    // count++;
+    // }
+    // res.add(count);
+    // while (!temp.isEmpty())
+    // stack.add(temp.pop());
+    // stack.add(arr[i]);
+    // }
+    // return res;
+    // }
+
+    public static ArrayList<Integer> calculateSpanMap(int[] arr) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(arr[0], 1);
+        res.add(1);
+
+        for (int i = 1; i < arr.length; i++) {
             int count = 1;
-            while (!stack.isEmpty() && stack.peek() <= arr[i]) {
-                temp.add(stack.pop());
-                count++;
+            int idx = i - 1;
+            int val = arr[idx];
+            while (val <= arr[i]) {
+                int ct = map.get(val);
+                count += ct;
+                idx -= ct;
+                if (idx < 0)
+                    break;
+                val = arr[idx];
             }
+            map.put(arr[i], count);
             res.add(count);
-            while (!temp.isEmpty())
-                stack.add(temp.pop());
-            stack.add(arr[i]);
         }
         return res;
     }
