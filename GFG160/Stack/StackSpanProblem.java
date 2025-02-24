98269d4d6da2301af7dc3b1834c97acf694b52ba
sanskar jain <sanskarj15@gmail.com>
2025-02-24 22:41:28 +0530

    leetcode commit

--- GFG160/Stack/StackSpanProblem.java
+++ GFG160/Stack/StackSpanProblem.java
@@ -20,10 +20,32 @@ public class StackSpanProblem {
                 count++;
             }
             res.add(count);
-            stack.add(arr[i]);
             while (!temp.isEmpty())
                 stack.add(temp.pop());
+            stack.add(arr[i]);
         }
         return res;
     }
 }
+
+
+// public static Stack<Integer> stack = new Stack<>();
+//     public static Stack<Integer> temp = new Stack<>();
+
+//     public StockSpanner() {
+//         this.stack.clear();
+//         this.temp.clear();
+//         this.res.clear();
+//     }
+    
+//     public int next(int price) {
+//         int count = 1;
+//         while (!stack.isEmpty() && stack.peek() < price) {
+//             temp.add(stack.pop());
+//             count++;
+//         }
+//         while (!temp.isEmpty())
+//             stack.add(temp.pop());
+//         stack.add(arr[i]);
+//         return count;
+// }
