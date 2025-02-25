b74d4c481ca7e58d1cde0ac652917bb4a0f4216e
sanskar jain <sanskarj15@gmail.com>
2025-02-25 12:57:47 +0530

    commit

---
+++ LeetcodeFeb/MostProfitablePathInATree.java
@@ -0,0 +1,12 @@
+package LeetcodeFeb;
+
+public class MostProfitablePathInATree {
+    public static void main(String[] args) {
+        int edges[][] = { { 0, 1 }, { 1, 2 }, { 1, 3 }, { 3, 4 } }, bob = 3, amount[] = { -2, 4, 2, -4, 6 };
+        System.out.println(mostProfitablePath(edges, bob, amount));
+    }
+
+    public static int mostProfitablePath(int[][] edges, int bob, int[] amount) {
+        return 0;
+    }
+}
