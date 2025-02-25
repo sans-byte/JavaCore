3cdfe19a56445efc5364bd694bc4785718896e54
sanskar jain <sanskarj15@gmail.com>
2025-02-25 13:17:13 +0530

    brute force

--- LeetcodeFeb/NumberOfSubArraysWithOddSum.java
+++ LeetcodeFeb/NumberOfSubArraysWithOddSum.java
@@ -2,26 +2,20 @@ package LeetcodeFeb;
 
 public class NumberOfSubArraysWithOddSum {
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 6 };
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
         System.out.println(numOfSubarrays(arr));
     }
 
-    public static void getAllSubArrays(int[] arr, int i, int sum, int[] count) {
-        if (i >= arr.length) {
-            if (sum % 2 != 0)
-                count[0]++;
-            return;
-        }
-
-        getAllSubArrays(arr, i + 1, sum + arr[i], count);
-        getAllSubArrays(arr, i + 1, sum, count);
-
-        return;
-    }
-
     public static int numOfSubarrays(int[] arr) {
-        int count[] = new int[] { 0 };
-        getAllSubArrays(arr, 0, 0, count);
-        return count[0];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                if (sum % 2 != 0)
+                    count++;
+            }
+        }
+        return count;
     }
 }
