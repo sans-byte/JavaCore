760a7903a8cc1e97ae664f0988e572e00002f8af
sans-byte <sanskarj15@gmail.com>
2025-02-26 12:45:06 +0530

    commit

---
+++ GFG160/Stack/MaximinOfMinimumForEveryWindowSize.java
@@ -0,0 +1,14 @@
+package GFG160.Stack;
+
+import java.util.ArrayList;
+
+public class MaximinOfMinimumForEveryWindowSize {
+    public static void main(String[] args) {
+        
+    }
+
+    public static ArrayList<Integer> maxOfMins(int[] arr) {
+        ArrayList<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
