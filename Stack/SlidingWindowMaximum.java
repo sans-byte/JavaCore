1e610eda5a1f0c85a78ce37c770bdbe68ad5ab0d
sanskar jain <sanskarj15@gmail.com>
2025-02-27 13:19:40 +0530

    commit

---
+++ Stack/SlidingWindowMaximum.java
@@ -0,0 +1,12 @@
+package Stack;
+
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int res[] = new int[nums.length - k];
+        return res;
+    }
+}
