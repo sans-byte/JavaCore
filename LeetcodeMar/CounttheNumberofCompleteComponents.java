2b7fbb8c13b3ec10d1900b9de86a7be73b6fca00
sans-byte <sanskarj15@gmail.com>
2025-03-22 21:58:04 +0530

    commit

--- LeetcodeMar/CounttheNumberofCompleteComponents.java
+++ LeetcodeMar/CounttheNumberofCompleteComponents.java
@@ -1,7 +1,21 @@
 package LeetcodeMar;
 
+import java.util.*;
+
 public class CounttheNumberofCompleteComponents {
     public static void main(String[] args) {
-        
+        int n = 6, edges[][] = { { 0, 1 }, { 0, 2 }, { 1, 2 }, { 3, 4 } };
+        System.out.println(countCompleteComponents(n, edges));
+    }
+
+    public static int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        return 0;
     }
 }
