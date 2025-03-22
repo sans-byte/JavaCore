20b790aa8f4c7b197cf5135b2c3eca845bdab900
sans-byte <sanskarj15@gmail.com>
2025-03-22 22:12:37 +0530

    commit

--- LeetcodeMar/CounttheNumberofCompleteComponents.java
+++ LeetcodeMar/CounttheNumberofCompleteComponents.java
@@ -8,6 +8,16 @@ public class CounttheNumberofCompleteComponents {
         System.out.println(countCompleteComponents(n, edges));
     }
 
+    public static void dfs(int u, List<List<Integer>> adj, int[] visited, int degreeSum, int nodes) {
+        visited[u] = 1;
+        nodes++;
+        degreeSum = adj.get(u).size();
+        for (int v : adj.get(u)) {
+            if (visited[v] == 0)
+                dfs(v, adj, visited, degreeSum, nodes);
+        }
+    }
+
     public static int countCompleteComponents(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++)
@@ -16,6 +26,17 @@ public class CounttheNumberofCompleteComponents {
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
-        return 0;
+
+        int visited[] = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (visited[i] != 0)
+                continue;
+            int degreeSum = 0, nodes = 0;
+            dfs(i, adj, visited, degreeSum, nodes);
+            if (degreeSum == nodes * (nodes - 1))
+                ans++;
+        }
+        return ans;
     }
 }
