9bbd856e46c9f9a712677f7d41e149a74f73b792
sans-byte <sanskarj15@gmail.com>
2025-03-24 21:41:20 +0530

    commit:

---
+++ LeetcodeMar/CountDaysWithoutMeetings.java
@@ -0,0 +1,13 @@
+package LeetcodeMar;
+
+public class CountDaysWithoutMeetings {
+    public static void main(String[] args) {
+        int days = 10, meetings[][] = { { 5, 7 }, { 1, 3 }, { 9, 10 } };
+        System.out.println(countDays(days, meetings));
+    }
+
+    public static int countDays(int days, int[][] meetings) {
+        
+        return 0;
+    }
+}
