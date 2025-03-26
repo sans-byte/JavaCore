a4b0f97790918df77165fb195c88b88782bf550b
sans-byte <sanskarj15@gmail.com>
2025-03-26 22:39:25 +0530

    commit

---
+++ LeetcodeMar/MinimumOperationstoMakeaUniValueGrid.java
@@ -0,0 +1,22 @@
+package LeetcodeMar;
+
+public class MinimumOperationstoMakeaUniValueGrid {
+    public static void main(String[] args) {
+        int grid[][] = { { 2, 4 }, { 6, 8 } }, x = 2;
+        System.out.println(minOperations(grid, x));
+    }
+
+    public static int minOperations(int[][] grid, int x) {
+        double remainder = grid[0][0] % x;
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                double value = grid[i][j] % x;
+                sum += grid[i][j];
+                if (value != remainder)
+                    return -1;
+            }
+        }
+        return 1;
+    }
+}
