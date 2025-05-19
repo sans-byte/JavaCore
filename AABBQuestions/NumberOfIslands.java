8608bc4f7a00437208d17e6c6d0236191d7ea8bd
sans-byte <sanskarj15@gmail.com>
2025-05-19 19:51:57 +0530

    commit

---
+++ AABBQuestions/NumberOfIslands.java
@@ -0,0 +1,17 @@
+package AABBQuestions;
+
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        char[][] grid = {
+                { '1', '1', '1', '1', '0' },
+                { '1', '1', '0', '1', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '0', '0', '0' }
+        };
+        System.out.println(numIslands(grid));
+    }
+
+    public static int numIslands(char[][] grid) {
+        return 0;
+    }
+}
